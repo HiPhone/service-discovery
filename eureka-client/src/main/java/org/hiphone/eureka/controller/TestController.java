f91c8dc414d2d36b821accbb52545f52600c7ea1
HiPhone <zhyzyhf@gmail.com>
2018-10-23 22:09:46 +0800

    添加maven-assembly打包插件

--- eureka-client/src/main/java/org/hiphone/eureka/controller/TestController.java
+++ eureka-client/src/main/java/org/hiphone/eureka/controller/TestController.java
@@ -2,8 +2,11 @@ package org.hiphone.eureka.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.hiphone.eureka.constants.ReturnCode;
+import org.hiphone.eureka.entity.ResultMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +20,19 @@ public class TestController {
 
     public static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @ResponseBody
     @GetMapping("/echo-test")
     @ApiOperation(value = "用于确认服务是否存活的接口", notes = "返回自身状态")
-    public String test() {
-        logger.info("Receive a request for testing eureka client");
+    public ResultMessage test() {
+        logger.info("Receive a request for testing " + applicationName);
 
-        return "Eureka Client is alive";
+        return new ResultMessage(
+                ReturnCode.SUCCESS.getCode(),
+                ReturnCode.SUCCESS.getMessage(),
+                applicationName + " is alive");
     }
 
 }
